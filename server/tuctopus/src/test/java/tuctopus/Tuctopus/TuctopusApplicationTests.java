2f1615b8fe00b13d657d671c54e0436175010ceb
복지희 <129582481+jettieb@users.noreply.github.com>
2024-09-12 14:19:39 +0900

    build: 백엔드 개발환경 설정

---
+++ server/tuctopus/src/test/java/tuctopus/Tuctopus/TuctopusApplicationTests.java
@@ -0,0 +1,13 @@
+package tuctopus.Tuctopus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuctopusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
