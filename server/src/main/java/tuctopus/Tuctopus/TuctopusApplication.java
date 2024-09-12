5b350351385775b0f0713791230d51cfb8e1d9c4
복지희 <129582481+jettieb@users.noreply.github.com>
2024-09-12 14:41:02 +0900

    build: 백엔드 개발환경 설정

---
+++ server/src/main/java/tuctopus/Tuctopus/TuctopusApplication.java
@@ -0,0 +1,13 @@
+package tuctopus.Tuctopus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuctopusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuctopusApplication.class, args);
+	}
+
+}
