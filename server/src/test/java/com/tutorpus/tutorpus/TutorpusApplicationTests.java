b4ccfa96ad234966148b68d89efec1e4aa75dd7c
Bokjihee <bokjihee@naver.com>
2024-11-05 10:05:32 +0900

    chore: 백엔드 개발환경 재설정

---
+++ server/src/test/java/com/tutorpus/tutorpus/TutorpusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorpus.tutorpus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorpusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
